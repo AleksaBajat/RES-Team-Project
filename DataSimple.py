f28f8ed040c426fbd0b091b987073cec6fa81cb8
anjapuskas <anjapuskas@hotmail.com>
2022-05-26 19:38:08 +0200

    Add DataSimple class

---
+++ DataSimple.py
@@ -0,0 +1,6 @@
+class DataSimple:
+    def __init__(id, potrosnja, korisnik, adresa):
+        self.id = id
+        self.potrosnja = potrosnja
+        self.korisnik = korisnik
+        self.adresa = adresa
