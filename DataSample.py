3d1837f1ee9fe616355ff1224e695f20b445fd64
Dragan Stancevic <draganstancevic15@gmail.com>
2022-06-02 18:24:17 +0200

    Added time to DataSample

--- DataSample.py
+++ DataSample.py
@@ -1,6 +1,7 @@
 class DataSample:
-    def __init__(self, id, potrosnja, korisnik, adresa):
+    def __init__(self, id, potrosnja, korisnik, adresa, vreme):
         self.id = id
         self.potrosnja = potrosnja
         self.korisnik = korisnik
         self.adresa = adresa
+        self.vreme = vreme
