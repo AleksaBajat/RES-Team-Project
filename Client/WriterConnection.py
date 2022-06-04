06e789d3e4d4dd911817653e87ca3548655600cd
Dragan Stancevic <draganstancevic15@gmail.com>
2022-06-04 18:14:37 +0200

    Changed ports, fixed writer bug

--- Client/WriterConnection.py
+++ Client/WriterConnection.py
@@ -25,21 +25,21 @@ def getMessage():
 
     message = DataSample(ID, potrosnja, korisnik, adresa)
     
-    return json.dumps(message.dict).encode("utf-8")
+    return json.dumps(message.__dict__).encode("utf-8")
 
 def connectToWriter():
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ("127.0.0.1", 10101)
+    server_address = ("127.0.0.1", 10000)
     print("connecting to " + str(server_address))
     sock.connect(server_address)
 
     try:   
         message = getMessage()
         print("sending " + str(message))
-        sock.send(message.encode("utf-8"))
+        sock.send(message)
            
     except Exception as e:
         print(e)
