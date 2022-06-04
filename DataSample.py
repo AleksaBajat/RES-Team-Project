719119a76019047f47eabbf2967fb70369b3dcd1
PeskanovJ <94849228+PeskanovJ@users.noreply.github.com>
2022-06-04 16:52:09 +0200

    Delete DataSample.py

--- DataSample.py
+++
@@ -1,7 +0,0 @@
-class DataSample:
-    def __init__(self, id, potrosnja, korisnik, adresa, vreme):
-        self.id = id
-        self.potrosnja = potrosnja
-        self.korisnik = korisnik
-        self.adresa = adresa
-        self.vreme = vreme
