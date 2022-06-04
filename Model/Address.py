7ca9c4e1e9022b7624042633b71b2246c52e13ca
AleksaBajat <aleksabajat2001@gmail.com>
2022-06-04 15:19:57 +0200

    Some minor changes to accomplish new hierarchical structure and added new Address class

---
+++ Model/Address.py
@@ -0,0 +1,6 @@
+class Address:
+    def __init__(self, country,city,street, street_number):
+        self.country = country
+        self.city = city
+        self.street = street
+        self.street_number = street_number
