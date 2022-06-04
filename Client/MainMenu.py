966d7fc2bef8f8874b98f36c8f2c1da322fb062d
Dragan Stancevic <draganstancevic15@gmail.com>
2022-06-04 17:29:49 +0200

    Expand manu

--- Client/MainMenu.py
+++ Client/MainMenu.py
@@ -10,7 +10,13 @@ def menu():
             connectToWriter()
         elif x == "r":
             print(
-                "Odaberite opciju citanja\n1 - Procitaj sve\n2 - Procitaj po odredjenom mesecu\n3 - Procitaj po korisniku\n4 - Procitaj po gradu")
+                "Odaberite opciju citanja\n" + 
+                "1 - Procitaj sve\n" + 
+                "2 - Procitaj po odredjenom mesecu\n" +
+                "3 - Procitaj po korisniku\n" + 
+                "4 - Procitaj po gradu" +
+                "5 - Procitaj po potrosnji vecoj od..." +
+                "6 - Procitaj po potrosnji manjoj od...")
             y = input()
             z = '0'
             if y == "2":
@@ -21,6 +27,12 @@ def menu():
                 z = input()
             elif y == "4":
                 print("Unesite grad")
+                z = input()
+            elif y == "5":
+                print("Unesite vrednost potrosnje")
+                z = input()
+            elif y == "6":
+                print("Unesite vrednost potrosnje")
                 z = input()
             connectToReader(y, z)
         elif x == "x":
