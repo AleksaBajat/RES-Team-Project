5ca9172700e14d4edf59755fa5d344a4e7467b76
PeskanovJ <94849228+PeskanovJ@users.noreply.github.com>
2022-06-04 12:40:29 +0200

    DataSapmle change
    Pulled changed DataSample with time from origin/Dragan and translated it.

--- Client/DataSample.py
+++ Client/DataSample.py
@@ -1,6 +1,7 @@
 class DataSample:
-    def __init__(self, unitId, consumption, userId, address):
+    def __init__(self, unitId, consumption, userId, address,time):
         self.unitId = unitId
         self.consumption = consumption
         self.userId = userId
         self.address = address
+        self.time = time
