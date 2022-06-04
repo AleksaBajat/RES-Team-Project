f49c0bb6e2264601c4399ddf11b5ba10dadd4ce4
PeskanovJ <94849228+PeskanovJ@users.noreply.github.com>
2022-06-04 17:46:02 +0200

    Made options in database connection
    Made options in database connection

--- Reader/CreateReply.py
+++ Reader/CreateReply.py
@@ -4,16 +4,24 @@ from ConnectToDatabase import *
 def getReply(option,parameter):
     if(option==1):
         string= getAll()
-    
+    elif(option==2):
+        string =getByMonth(parameter)
+    elif(option==3):
+        string =getByClientId(parameter)
+    elif(option==4):
+        string=getByCity(parameter)
+    elif(option==5):
+        string = getByPowerConsumptionAbove(parameter)
+    elif(option==6):
+        string = getByPowerConsumptionBelow(parameter)
     reply = open_connection_and_reply(string)
-    return "aaa"
+    return reply
 
 def getAll():
     return "Select * from Data"
 
 def getByMonth(month):
     return
-
 def getByAddress(address):
     return
 
@@ -27,4 +35,4 @@ def getByCity(city):
     return
 
 def getByClientId(userId):
-    return
+    return "SELECT * FROM Data WHERE user_id ='"+userId+"'"
