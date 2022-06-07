108f55737c245d3f4b80d85ef70979f334f1b3b1
Dragan Stancevic <draganstancevic15@gmail.com>
2022-06-07 18:54:01 +0200

    Added functionality for sending back a return message

--- Writer/Writer.py
+++ Writer/Writer.py
@@ -14,31 +14,37 @@ ReceivePort = 10000
 SendHost = "127.0.0.1"
 SendPort = 20000
 
-
 def multi_threaded_connection(connection):
     with connection:
-        data = conn.recv(1024)
+        data = connection.recv(1024)
 
-        print(data)
         sample = pickle.loads(data)
-        print(str(sample))        # radi provere
+        print(str(sample))
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:                            # slanje ka Dump buffer-u
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:                            # sending towards DumpBufer
             try:
                 s.connect((SendHost, SendPort))
                 s.send(pickle.dumps(sample))
+                value = s.recv(1024)
+                value = pickle.loads(value)
+                print(value)
+                connection.send(pickle.dumps(value))                    # sending return value back
+
             except Exception as e:
                 print(e)
             finally:
                 s.close()
                 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((ReceiveHost, ReceivePort))
-    while(True):
-        s.listen()
-        conn, addr = s.accept()
-        print(f"Connected by {addr}")
-        start_new_thread(multi_threaded_connection, (conn, ))
-        
+def createListener():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ReceiveHost, ReceivePort))
+        while(True):
+            s.listen()
+            conn, addr = s.accept()
+            print(f"Connected by {addr}")
+            start_new_thread(multi_threaded_connection, (conn, ))
 
+
+print("Writer started:")
+createListener()
