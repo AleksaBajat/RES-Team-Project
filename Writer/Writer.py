b788fb6a911101416fe690df9947bb391c602b5d
Dragan Stancevic <draganstancevic15@gmail.com>
2022-06-07 19:58:01 +0200

    Pack all to smaller functions and made it possible for testing

--- Writer/Writer.py
+++ Writer/Writer.py
@@ -1,3 +1,4 @@
+from random import sample
 import sys
 sys.path.append('../')
 
@@ -14,37 +15,51 @@ ReceivePort = 10000
 SendHost = "127.0.0.1"
 SendPort = 20000
 
+    
+
+def receiveData(connection):
+    data = connection.recv(1024)
+    sample = pickle.loads(data)
+    print(str(sample))
+    return sample
+
+
+def sendData(sample):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:                            # sending towards DumpBufer
+        try:
+            s.connect((SendHost, SendPort))
+            s.send(pickle.dumps(sample))
+            value = s.recv(1024)
+            value = pickle.loads(value)
+            print(value)
+            
+        except Exception as e:
+            print(e)
+        finally:
+            s.close()
+            return value
+
+
 def multi_threaded_connection(connection):
-    with connection:
-        data = connection.recv(1024)
+    sample = receiveData(connection)
+    return_message = sendData(sample)
+    connection.send(pickle.dumps(return_message))                    # sending return value back
+    connection.close()
 
-        sample = pickle.loads(data)
-        print(str(sample))
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:                            # sending towards DumpBufer
-            try:
-                s.connect((SendHost, SendPort))
-                s.send(pickle.dumps(sample))
-                value = s.recv(1024)
-                value = pickle.loads(value)
-                print(value)
-                connection.send(pickle.dumps(value))                    # sending return value back
+def createListener(s):
+    s.listen()
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
+    return (conn, addr)
 
-            except Exception as e:
-                print(e)
-            finally:
-                s.close()
-                
-
-def createListener():
+def startService():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((ReceiveHost, ReceivePort))
         while(True):
-            s.listen()
-            conn, addr = s.accept()
-            print(f"Connected by {addr}")
+            conn, addr = createListener(s)
             start_new_thread(multi_threaded_connection, (conn, ))
-
+            
 
 print("Writer started:")
-createListener()
+startService()
