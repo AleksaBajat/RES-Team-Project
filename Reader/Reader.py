fb3c49e7f2382a86806fcd8703f525db96a81951
PeskanovJ <94849228+PeskanovJ@users.noreply.github.com>
2022-06-12 19:36:46 +0200

    Test for Reader/Reader.py
    Reader.py functions tested in ReaderTest

--- Reader/Reader.py
+++ Reader/Reader.py
@@ -1,25 +1,24 @@
-from optparse import Option
-from sqlite3 import paramstyle
-from CreateQuery import *
 import sys
 import pickle
-
-sys.path.append('../')
-
 import json
 import socket
+sys.path.append('../')
+
+from Reader.CreateQuery import *
 from _thread import *
 
-from Model.DataSample import *
 
 ReceiveHost = "127.0.0.1" 
 ReceivePort = 40000
 
 HistoricalHost="127.0.0.1"
 HistoricalPort=30000
+    
+def get_socket():
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def getFromHistorical(string):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def get_from_historical(string):
+    sock=get_socket()
     server_address = (HistoricalHost, HistoricalPort)
     print("connecting to Historical on address " + str(HistoricalHost)+":"+str(HistoricalPort))
     sock.connect(server_address)
@@ -27,40 +26,44 @@ def getFromHistorical(string):
     try:
         sock.send(pickle.dumps(string.encode("utf-8")))
         reply = sock.recv(1024)
-        return reply
 
     except Exception as e:
         print(e)
+        return "ERROR"
     finally:
-        print('closing socet for Historical')
+        print('closing socket for Historical')
         sock.close()
+        return reply
 
+
+def get_params(data):
+    option=data.split(',')[0]
+    parameter=data.split(',')[1]
+    parameter=parameter[1:]
+    return option,parameter
 
 def multi_threaded_connection(connection): 
     with connection:
         while True:
-            data = conn.recv(1024)
+            data = connection.recv(1024)
             if not data:
                 break
-            data=(data.decode("utf-8"))                                                          
-            option=data.split(',')[0]
-            parameter=data.split(',')[1]
-            parameter=parameter[1:]
+            data=(data.decode("utf-8"))                                                                     
+            option,parameter=get_params(data)
             string=get_query(option,parameter)
-            
-            reply=getFromHistorical(string)
-
-            conn.sendall(reply)
+            reply=get_from_historical(string)
+            connection.sendall(reply)
         
       
+def start_reader():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ReceiveHost, ReceivePort))
+        print("Reader started!")
+        while(True):
+            s.listen()
+            conn, addr = s.accept()
+            print(f"Connected by {addr}")
+            start_new_thread(multi_threaded_connection, (conn, ))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((ReceiveHost, ReceivePort))
-    print("Reader started!")
-    while(True):
-        s.listen()
-        conn, addr = s.accept()
-        print(f"Connected by {addr}")
-        start_new_thread(multi_threaded_connection, (conn, ))
-        
-
+if __name__ == '__main__':
+    start_reader()
--- Tests/ReaderTest.py
+++ Tests/ReaderTest.py
@@ -1,6 +1,10 @@
+from socket import socket
 import sys
+from unittest import mock
+from unittest.mock import  MagicMock, patch
 sys.path.append('../')
 from Reader.CreateQuery import *
+from Reader.Reader import *
 import unittest
 
 
@@ -12,7 +16,21 @@ class TestCreateQuery(unittest.TestCase):
         self.assertEqual(get_query("4","Becej"), "SELECT * FROM meterReadings WHERE city = 'Becej'")
         self.assertEqual(get_query("5",5000), "SELECT * FROM meterReadings WHERE consumption >= 5000")
         self.assertEqual(get_query("6",5000), "SELECT * FROM meterReadings WHERE consumption < 5000")
+
+
+class TestReader(unittest.TestCase):
+    
+    def test_get_params(self):
+        self.assertEqual(('1','0'),get_params("1, 0"))
+
+    @patch('Reader.Reader.get_socket')
+    def test_get_from_historical(self,mock_get_socket):
+        mock_socket = MagicMock(socket.socket)
+        mock_socket.recv = MagicMock(return_value = "aaa")
+        mock_get_socket.return_value = mock_socket
+
         
+        self.assertEqual(get_from_historical("bbb"),"aaa")
 
 if __name__ == '__main__':
     unittest.main()
